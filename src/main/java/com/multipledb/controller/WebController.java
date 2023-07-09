26ffe67f70f08ad82b6b295bfcfb457b2cdd0bae
Lokesh Parihar <44647778+lokeshparihar100@users.noreply.github.com>
2023-07-09 23:55:58 +0530

    Add Comments

--- src/main/java/com/multipledb/controller/WebController.java
+++ src/main/java/com/multipledb/controller/WebController.java
@@ -18,68 +18,86 @@ import com.multipledb.model.user.User;
 import com.multipledb.repository.book.BookRepository;
 import com.multipledb.repository.user.UserRepository;
 
+/*
+* WebController for all requests with RestController
+*/
+
 @RestController
 public class WebController {
 
 	@Autowired
-	private BookRepository bookRepository;
+	private BookRepository bookRepository;				// Create BookRepository Class Instance with Autowired
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userRepository;				// Create UserRepository Class Instance with Autowired
 
+	// PostConstruct is use to run just after Tomcat server start
 	@PostConstruct
 	public void addData2DB() {
-		userRepository.saveAll(Stream.of(new User(744, "John"), new User(455, "Pitter")).collect(Collectors.toList()));
+	// Save the data into MySQL datasource1 schema if table not exist then it will create automatically by 'hibernate.hbm2ddl.auto' property 'update'
+		userRepository.saveAll(Stream.of(new User(101, "John"), new User(102, "Pitter"), new User(103, "Harry"), new User(104, "Kim"),
+			new User(105, "Tom"), new User(106, "Ana"), new User(107, "Hema"), new User(108, "Tiger"), 
+			new User(109, "Angela"), new User(110, "Hero"), new User(111, "Mac"), new User(112, "Zen")
+						).collect(Collectors.toList()));
 		bookRepository.saveAll(
 				Stream.of(new Book(111, "Core Java"), new Book(222, "Spring Boot")).collect(Collectors.toList()));
 	}
-
-	@GetMapping("/getUsers")
+	// URL: http://localhost:8080/getAllUsers
+	@GetMapping("/getAllUsers")
 	public List<User> getUsers() {
 		return userRepository.findAll();
 	}
 
-	@GetMapping("/getBooks")
+	// URL: http://localhost:8080/getAllBooks
+	@GetMapping("/getAllBooks")
 	public List<Book> getBooks() {
 		return bookRepository.findAll();
 	}
-	
-	@GetMapping("getUsers/{id}")
-	public Optional<User> getUserById(@PathVariable Integer id){
-		return userRepository.findById(id);
-	}
-	
+
+	// URL: http://localhost:8080/getFirstUser
 	@GetMapping("getFirstUser")
 	public User findFirstByOrderByIdAsc() {
 		return userRepository.findFirstByOrderByIdAsc();
 	}
-	
-	@GetMapping("getTopUser")
+
+	// URL: http://localhost:8080/getFirstUserAlternate
+	@GetMapping("getFirstUserAlternate")
 	public User findTopByOrderByIdAsc() {
 		return userRepository.findTopByOrderByIdAsc();
 	}
-	
-	@GetMapping("getUsersbyId")
+
+	// URL: http://localhost:8080/getUsersById
+	@GetMapping("getUsersById")
 	public List<User> findByOrderByIdAsc() {
 		return userRepository.findByOrderByIdAsc();
 	}
-	
-	@GetMapping("getUserss/{name}")
+
+	// URL: http://localhost:8080/getUsers/101
+	@GetMapping("getUsers/{id}")
+	public Optional<User> getUserById(@PathVariable Integer id){
+		return userRepository.findById(id);
+	}
+
+	// URL: http://localhost:8080/getUserIgnoreCase/John
+	@GetMapping("getUserIgnoreCase/{name}")
 	public List<User> findByUserNameIgnoreCase(@PathVariable String name) {
 		return userRepository.findByUserNameIgnoreCase(name);
 	}
-	
-	@GetMapping("getUser/{name}")
+
+	// URL: http://localhost:8080/getUserByName/John
+	@GetMapping("getUserByName/{name}")
 	public List<User> findByUserNameOrderByIdAsc(@PathVariable String name) {
 		return userRepository.findByUserNameOrderByIdAsc(name);
 	}
-	
+
+	// URL: http://localhost:8080/getUserOrder/John
 	@GetMapping("getUserOrder/{name}")
 	public List<User> findByUserName(@PathVariable String name) {
 		Sort sortBy = Sort.by(new Sort.Order(Sort.Direction.ASC, "id"));
 		return userRepository.findByUserName(name, sortBy);
 	}
-	
+
+	// URL: http://localhost:8080/getUserLimit/5
 	@GetMapping("getUserLimit/{num}")
 	public List<User> findOrderedByIdLimitedTo(@PathVariable Integer num) {
 		return userRepository.findOrderedByIdLimitedTo(num);
