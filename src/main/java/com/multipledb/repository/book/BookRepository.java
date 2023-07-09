bb2266a8528993d0dfc2a0584611572ba88f8c8e
Lokesh Parihar <44647778+lokeshparihar100@users.noreply.github.com>
2023-07-10 00:11:24 +0530

    Add comments

--- src/main/java/com/multipledb/repository/book/BookRepository.java
+++ src/main/java/com/multipledb/repository/book/BookRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.multipledb.model.book.Book;
 
+/*
+* BookRepository interface using JpaRepository<Entity, TypeOf @Id>
+*/
+
 public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
