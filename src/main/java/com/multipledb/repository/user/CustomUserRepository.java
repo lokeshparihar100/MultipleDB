b4cde891133bad15c13ceed3980b0290f466cd91
Lokesh Parihar <44647778+lokeshparihar100@users.noreply.github.com>
2023-07-10 00:22:39 +0530

    Add comments

--- src/main/java/com/multipledb/repository/user/CustomUserRepository.java
+++ src/main/java/com/multipledb/repository/user/CustomUserRepository.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import com.multipledb.model.user.User;
 
+/*
+* CustomUserRepository interface for Limit query
+*/
+
 public interface CustomUserRepository {
 
-	List<User> findOrderedByIdLimitedTo(int limit);
+	List<User> findOrderedByIdLimitedTo(int limit);	   // It will extract the number of Users which is given in Limit
 }
